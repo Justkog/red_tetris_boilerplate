3f82829f43af8423ba1fa930cdccbe44035065c2
Justkog <jblondea@student.42.fr>
2018-07-16 21:05:12 +0200

    static front - tetrimino drawing

--- src/client/components/board/board.js
+++ src/client/components/board/board.js
@@ -5,25 +5,55 @@ import Tetrimino from "../tetrimino/tetrimino";
 import * as R from 'ramda';
 
 const columnNumbers = R.range(0, 10);
-const listColumns = columnNumbers.map((number) =>
-	<div key={number.toString()} style={{flex: 1, textAlign: 'center', color: 'white', borderStyle: 'dashed', borderWidth: '2px', borderColor: 'orange'}}></div>
-);
+// const listColumns = columnNumbers.map((number) =>
+// 	<div key={number.toString()} style={{flex: 1, textAlign: 'center', color: 'white', borderStyle: 'dashed', borderWidth: '2px', borderColor: 'orange', position: 'relative'}}>
+// 		{ number == 2 && <Tetrimino />}
+// 	</div>
+// );
 
 const rowNumbers = R.range(0, 20);
-const listRows = rowNumbers.map((number) =>
-	<div key={number.toString()} style={{flex: 1, display: 'flex', flexDirection: 'row'}}>
-		{listColumns}
-	</div>
-);
+// const listRows = rowNumbers.map((number) =>
+// 	<div key={number.toString()} style={{flex: 1, display: 'flex', flexDirection: 'row'}}>
+// 		{listColumns}
+// 	</div>
+// );
 
+const Cell = (props) => {
+	return (
+		<div style={{flex: 1, textAlign: 'center', color: 'white', borderStyle: 'dashed', borderWidth: '2px', borderColor: 'orange', position: 'relative'}}>
+			{props.children}
+		</div>
+	);
+}
+
+const Row = (props) => {
+	return (
+		<div style={{flex: 1, display: 'flex', flexDirection: 'row'}}>
+			{props.children}
+		</div>
+	);
+}
 
 export const Board = () => {
-  return (
-	<Container>
-	    <div className="board" style={{display: 'flex', flexDirection: 'column'}}>
-			{/* <Tetrimino/> */}
-			{listRows}
-		</div>
-	</Container>
-  )
+	const testListColumns = columnNumbers.map((number) =>
+		<Cell key={number.toString()}>
+			{ number == 2 && <Tetrimino/>}
+		</Cell>
+	);
+	const listColumns = columnNumbers.map((number) =>
+		<Cell key={number.toString()}>
+		</Cell>
+	);
+	const listRows = rowNumbers.map((number) =>
+		<Row key={number.toString()}>
+			{ number == 3 ? testListColumns : listColumns}
+		</Row>
+	);
+	return (
+		<Container>
+			<div className="board" style={{display: 'flex', flexDirection: 'column'}}>
+				{listRows}
+			</div>
+		</Container>
+	)
 }
--- src/client/components/tetrimino/tetrimino.js
+++ src/client/components/tetrimino/tetrimino.js
@@ -1,9 +1,70 @@
 import React from 'react';
+import * as R from 'ramda';
+
+const columnNumbers = R.range(0, 3);
+const rowNumbers = R.range(0, 3);
+
+const tetriForm = [
+	[0, 1, 0],
+	[0, 1, 0],
+	[1, 1, 0],
+];
+
+const exist = (cellValue) => {
+	return cellValue == 1 ? true : false;
+}
+
+const defaultCellStyle = {
+	flex: 1,
+	textAlign: 'center',
+	color: 'white',
+	borderStyle: 'dashed',
+	borderWidth: '4px',
+	borderColor: 'transparent',
+}
+
+const cellStyle = (exist) => {
+	if (!exist) {
+		return defaultCellStyle;
+	}
+	return R.merge(defaultCellStyle, {borderColor: 'white', backgroundColor: 'var(--pink)'});
+}
+
+const tetriCells = (values) => {
+	const listCells = columnNumbers.map((number) => {
+		return (
+			<div key={number.toString()} style={cellStyle(exist(values[number]))}>
+				
+			</div>
+		);
+	});
+	return listCells;
+}
+
+const Grid = (props) => {
+	const listRows = rowNumbers.map((number) =>
+		<div key={number.toString()} style={{flex: 1, display: 'flex', flexDirection: 'row', height: '33.33%'}}>
+			{tetriCells(tetriForm[number])}
+		</div>
+	);
+	return (
+		<div style={{ height: '100%'}}>
+			{listRows}
+		</div>
+	);
+}
 
 const Tetrimino = () => {
 	return (
-		<div>
-			Tetri
+		<div style={{
+			width: 'calc(300% + 6 * 2px)',
+			height: 'calc(300% + 6 * 2px)',
+			top: 'calc(-100% - 3 * 2px)',
+			left: 'calc(-100% - 3 * 2px)',
+			position: 'absolute',
+			zIndex: 100,
+		}}>
+			<Grid/>
 		</div>
 	);
 }
