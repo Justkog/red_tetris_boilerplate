c9450fc538acd38c56dcafb203da29bd4510e137
Justkog <jblondea@student.42.fr>
2018-07-23 20:34:46 +0200

    state listener ok

--- src/client/actions/gameEvents.js
+++ src/client/actions/gameEvents.js
@@ -1,7 +1,16 @@
+import { Observable, Subject, ReplaySubject, from} from 'rxjs'
+import { map } from 'rxjs/operators';
+
 export const LISTEN_BOARD_UPDATE = 'LISTEN_BOARD_UPDATE'
 
 export const listenBoardUpdate = (store) => {
 	return (dispatch, getState) => {
+		// const observalbe = Observable.
+		// Observable.from()
+		const observable = from(store)
+		observable
+		// .pipe(map(state => ({ fromRx: true, ...state })))
+		.subscribe(state => console.dir(state))
 		// const observable = Rx.Observable.from(store)
 	}
 }
