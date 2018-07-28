bb8dc53167cd10ed3f4278c48b061ba92692c24a
Justkog <j_c_blondeau@hotmail.com>
2018-07-28 17:51:26 +0200

    import update for prod concern

--- src/client/epics/root.js
+++ src/client/epics/root.js
@@ -2,7 +2,7 @@ import { combineEpics, ofType } from 'redux-observable';
 import { LINES_DELETE, sendBoardUpdate } from '../actions/board';
 import { INDESTRUCTIBLE_LINES_ADD } from '../../server/tools/constants';
 import { TETRIMINO_REMOVE, TETRIMINO_SEAL } from '../actions/tetrimino';
-import { map } from '../../../node_modules/rxjs/operators';
+import { map } from 'rxjs/operators';
 
 const sendBoardEpic = (action$, state$) => action$.pipe(
     ofType(
