eb7bf5362ca630585ecf0fa3d9f0b4fabd63facd
oren <oren.carta-lag@hotmail.fr>
2018-07-29 02:47:56 +0200

    reset game on game end + reset player on new game

--- src/server/tools/engine.js
+++ src/server/tools/engine.js
@@ -1,5 +1,5 @@
 import * as constants from './constants';
-import { game_creation, game_join, game_start, game_creation_solo, player_end, game_leave } from './engine/game';
+import { game_join, game_start, game_creation_solo, player_end, game_leave } from './engine/game';
 import { next_tetri } from './engine/tetri';
 import { board_update, user_line_delete } from './engine/board';
 import { login, logout } from './engine/connection';
@@ -9,7 +9,6 @@ export const initEngine = (io, supervisor) => {
     login(socket, supervisor);
     logout(socket, supervisor);
 
-    game_creation(socket, supervisor);
     game_creation_solo(socket, supervisor);
     game_start(socket, supervisor);
     game_join(socket, supervisor);
