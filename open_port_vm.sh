c3ac8e87b57be7808163172f3de60c324c55db74
oren <oren.carta-lag@hotmail.fr>
2018-08-01 19:37:01 +0200

    fix player receive update after fail game_join + rename script

--- open_port_vm.sh
+++ open_port_vm.sh
@@ -8,5 +8,3 @@ export IP_MACHINE=`Docker-machine ip $NAME_MACHINE`
 export IP_ONLINE=`ifconfig en0 | grep inet\ |  awk -F' ' '{print $2}'`
 
 VBoxManage controlvm "$NAME_MACHINE" natpf1 "redtetris-server,tcp,$IP_ONLINE,8080,$IP_MACHINE,8080"
-
-docker-compose up
