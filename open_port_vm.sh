d152d8d2217c34bc82f97a00c0465205eeb7b2f4
Justkog <jblondea@student.42.fr>
2018-08-04 18:01:56 +0200

    ready for set as finished

--- open_port_vm.sh
+++ open_port_vm.sh
@@ -7,4 +7,5 @@ eval $(docker-machine env $NAME_MACHINE)
 export IP_MACHINE=`Docker-machine ip $NAME_MACHINE`
 export IP_ONLINE=`ifconfig en0 | grep inet\ |  awk -F' ' '{print $2}'`
 
-VBoxManage controlvm "$NAME_MACHINE" natpf1 "redtetris-server,tcp,$IP_ONLINE,8080,$IP_MACHINE,8080"
+VBoxManage controlvm "$NAME_MACHINE" natpf1 "redtetris-server-dev,tcp,$IP_ONLINE,8080,$IP_MACHINE,8080"
+VBoxManage controlvm "$NAME_MACHINE" natpf1 "redtetris-server-prod,tcp,$IP_ONLINE,3004,$IP_MACHINE,3004"
